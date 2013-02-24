c21a57d074b6d5c546795f6665e8b1f08fcac5d2
irhete <irene.teinemaa@gmail.com>
2013-02-24 02:02:33 +0200

    application form

--- Valimised/src/com/valimised/client/ApplicationForm.java
+++ Valimised/src/com/valimised/client/ApplicationForm.java
@@ -3,44 +3,160 @@ package com.valimised.client;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class ApplicationForm extends Composite {
+	FlexTable table;
+	Label firstNameLabel;
+	TextBox firstNameBox;
+	Label lastNameLabel;
+	TextBox lastNameBox;
+	Label personalCodeLabel;
+	TextBox personalCodeBox;
+	Label partyLabel;
+	ListBox partyBox;
+	Label areaLabel;
+	ListBox areaBox;
+	Label addressLabel;
+	TextArea addressArea;
+	Label phoneLabel;
+	TextBox phoneBox;
+	Button nextButton;
 
 	public ApplicationForm() {
 		VerticalPanel mainPanel = new VerticalPanel();
 		initWidget(mainPanel);
 
-		FlexTable table = new FlexTable();
-		Label firstNameLabel = new Label("Eesnimi");
-		TextBox firstNameBox = new TextBox();
-		Label lastNameLabel = new Label("Perekonnanimi");
-		TextBox lastNameBox = new TextBox();
-		Label personalCodeLabel = new Label("Isikukood");
-		TextBox personalCodeBox = new TextBox();
+		table = new FlexTable();
+		table.getFlexCellFormatter().setColSpan(7, 0, 2);
 
+		createWidgets();
+
+		setStyleNames();
+
+		addLabelsToTable();
+		addWidgetsToTable();
+
+		partyBox.addItem("Vali kuuluvus");
+		partyBox.addItem("Reformierakond");
+		partyBox.addItem("SDE");
+		partyBox.addItem("Keskerakond");
+		partyBox.addItem("IRL");
+		partyBox.addItem("Üksikkandidaat");
+
+		areaBox.addItem("Tartu linn");
+		areaBox.addItem("Tallinn");
+		areaBox.addItem("Rapla");
+		areaBox.addItem("Narva");
+
+		firstNameBox.setText("Mari");
+		lastNameBox.setText("Maripuu");
+		personalCodeBox.setText("44910220236");
+
+		setDisabled();
+
+		mainPanel.add(table);
+	}
+
+	private void setDisabled() {
+		firstNameBox.setEnabled(false);
+		lastNameBox.setEnabled(false);
+		personalCodeBox.setEnabled(false);
+	}
+
+	private void addLabelsToTable() {
 		table.setWidget(0, 0, firstNameLabel);
-		table.setWidget(0, 1, firstNameBox);
 		table.setWidget(1, 0, lastNameLabel);
-		table.setWidget(1, 1, lastNameBox);
 		table.setWidget(2, 0, personalCodeLabel);
-		table.setWidget(2, 1, personalCodeBox);
+		table.setWidget(3, 0, partyLabel);
+		table.setWidget(4, 0, areaLabel);
+		table.setWidget(5, 0, addressLabel);
+		table.setWidget(6, 0, phoneLabel);
+	}
 
-		Button candidates = new Button("Kandidaadid");
-		candidates.addClickHandler(new ClickHandler() {
+	private void addWidgetsToTable() {
+		table.setWidget(0, 1, firstNameBox);
+		table.setWidget(1, 1, lastNameBox);
+		table.setWidget(2, 1, personalCodeBox);
+		table.setWidget(3, 1, partyBox);
+		table.setWidget(4, 1, areaBox);
+		table.setWidget(5, 1, addressArea);
+		table.setWidget(6, 1, phoneBox);
+		table.setWidget(7, 0, nextButton);
+	}
+
+	private void setStyleNames() {
+		firstNameBox.setStyleName("applicationForm");
+		lastNameBox.setStyleName("applicationForm");
+		personalCodeBox.setStyleName("applicationForm");
+		partyBox.setStyleName("applicationForm");
+		areaBox.setStyleName("applicationForm");
+		addressArea.setStyleName("applicationForm");
+		phoneBox.setStyleName("applicationForm");
+		nextButton.setStyleName("applicationFormRightButton");
+	}
+
+	private void createWidgets() {
+		firstNameLabel = new Label("Eesnimi");
+		firstNameBox = new TextBox();
+		lastNameLabel = new Label("Perekonnanimi");
+		lastNameBox = new TextBox();
+		personalCodeLabel = new Label("Isikukood");
+		personalCodeBox = new TextBox();
+		partyLabel = new Label("Erakondlik kuuluvus");
+		partyBox = new ListBox();
+		areaLabel = new Label("Piirkond");
+		areaBox = new ListBox();
+		addressLabel = new Label("Aadress");
+		addressArea = new TextArea();
+		phoneLabel = new Label("Telefon");
+		phoneBox = new TextBox();
+		nextButton = new Button("Edasi");
+		nextButton.addClickHandler(new ClickHandler() {
 
 			@Override
 			public void onClick(ClickEvent event) {
-				ContentContainer.getInstance().setContent(new Candidates());
+				setToLabels();
 			}
 		});
+	}
 
-		mainPanel.add(table);
-		mainPanel.add(candidates);
+	protected void setToLabels() {
+		table.setWidget(0, 1, new Label(firstNameBox.getText()));
+		table.setWidget(1, 1, new Label(lastNameBox.getText()));
+		table.setWidget(2, 1, new Label(personalCodeBox.getText()));
+		table.setWidget(3, 1,
+				new Label(partyBox.getItemText(partyBox.getSelectedIndex())));
+		table.setWidget(4, 1,
+				new Label(areaBox.getItemText(partyBox.getSelectedIndex())));
+		table.setWidget(5, 1, new Label(addressArea.getText()));
+		table.setWidget(6, 1, new Label(phoneBox.getText()));
+
+		CheckBox confirmBox = new CheckBox(
+				"Kinnitan, et soovin kandideerida kohalikel valimistel");
+		table.setWidget(7, 0, confirmBox);
+
+		Button previousButton = new Button("Tagasi");
+		previousButton.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				addWidgetsToTable();
+				table.removeRow(8);
+			}
+		});
+		Button signButton = new Button("Allkirjastama");
+		signButton.setStyleName("applicationFormRightButton");
+
+		table.setWidget(8, 0, previousButton);
+		table.setWidget(8, 1, signButton);
 	}
 
 }
