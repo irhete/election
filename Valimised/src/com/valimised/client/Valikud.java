0f9409a3bfb86f17fcfe7e252beba5f1be07415a
irhete <irene.teinemaa@gmail.com>
2013-02-26 16:40:20 +0200

    suurused again

--- Valimised/src/com/valimised/client/Valikud.java
+++ Valimised/src/com/valimised/client/Valikud.java
@@ -5,32 +5,34 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.InlineHTML;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class Valikud extends Composite {
 
 	public Valikud() {
-		FlowPanel flowPanel = new FlowPanel();
-		initWidget(flowPanel);
-		flowPanel.addStyleName("valikud");
+		VerticalPanel verticalPane = new VerticalPanel();
+		initWidget(verticalPane);
+		verticalPane.addStyleName("valikud");
 
 		HorizontalPanel mainPanel = new HorizontalPanel();
+		mainPanel.addStyleName("chosenCandidatePanel");
 
 		InlineHTML html = new InlineHTML(
 				"<p>Olete hääletanud kandidaadi <a href='#'>Andres Tamm</a> poolt.</p>");
 		html.addDomHandler(new ClickHandler() {
-	        @Override
-	        public void onClick(ClickEvent event) {
-	            Element element =  event.getNativeEvent().getEventTarget().cast();
-	            if(element.getTagName().equals("A")) {
-	            	ContentContainer.getInstance().setContent(
+			@Override
+			public void onClick(ClickEvent event) {
+				Element element = event.getNativeEvent().getEventTarget()
+						.cast();
+				if (element.getTagName().equals("A")) {
+					ContentContainer.getInstance().setContent(
 							new Candidate("111"));
-	            }
+				}
 
-	        }
-	    }, ClickEvent.getType());
+			}
+		}, ClickEvent.getType());
 		Button cancel = new Button("Tühista");
 		cancel.addStyleName("cancelButton");
 		Button addCandidate = new Button("Lisa end kandidaadiks");
@@ -45,14 +47,11 @@ public class Valikud extends Composite {
 			}
 		});
 
-		mainPanel.setSpacing(20);
-		mainPanel.setHeight("30px");
-
 		mainPanel.add(html);
 		mainPanel.add(cancel);
 
-		flowPanel.add(mainPanel);
-		flowPanel.add(addCandidate);
+		verticalPane.add(mainPanel);
+		verticalPane.add(addCandidate);
 	}
 
 }
