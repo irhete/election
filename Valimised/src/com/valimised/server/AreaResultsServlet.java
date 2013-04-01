6813d53ac673cbdf3c2b7004dbeed7f6e96cbc3c
Marti Laast <martilaast@gmail.com>
2013-04-01 21:00:06 +0300

    Tried getting area results working. Needs debugging.

--- Valimised/src/com/valimised/server/AreaResultsServlet.java
+++ Valimised/src/com/valimised/server/AreaResultsServlet.java
@@ -17,6 +17,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.appengine.api.rdbms.AppEngineDriver;
 import com.google.gson.Gson;
 import com.valimised.shared.AreaResult;
+import com.valimised.shared.Data;
 
 @SuppressWarnings("serial")
 public class AreaResultsServlet extends HttpServlet {
@@ -29,23 +30,21 @@ public class AreaResultsServlet extends HttpServlet {
 		try {
 			DriverManager.registerDriver(new AppEngineDriver());
 
-			// returns particular area results
-			// currently only returns Tallinn3 area.
-			// String area = request.getParameter("");
-			String query = "select firstName, lastName, id, partyName, votes from candidate"
-					+ " join election.party on party = party.PartyID where area = 3 order by lastName, firstName";
+			String query = "SELECT firstName, lastName, id, party, votes FROM candidate WHERE area = ?";
 
 			c = DriverManager
 					.getConnection("jdbc:google:rdbms://e-election-app:instance2/election");
-
+			String areaId = request.getParameter("areaId");
+			System.out.println(areaId);
 			PreparedStatement statement = c.prepareStatement(query);
+			statement.setString(1, areaId);
 			ResultSet tableRows = statement.executeQuery();
 			List<AreaResult> results = new ArrayList<AreaResult>();
 			while (tableRows.next()) {
 				AreaResult areaResult = new AreaResult(tableRows.getString("firstName")
 						+ " " + tableRows.getString("lastName"),
 						tableRows.getInt("id"),
-						tableRows.getString("partyName"),
+						Data.parties[tableRows.getInt("party")],
 						tableRows.getInt("votes"));
 				results.add(areaResult);
 			}
