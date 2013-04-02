ba3b4483fe9e11526bd9caec8e8ab0c9f506ef3a
irhete <irene.teinemaa@gmail.com>
2013-04-02 12:32:51 +0300

    re-fix area results table

--- Valimised/war/javascript/results.js
+++ Valimised/war/javascript/results.js
@@ -85,45 +85,6 @@ function createAreaResultsTable(areaId) {
 	});
 }
 
-function createAreaResultsTable(partyId) {
-	$.ajax({
-		url:'/results/area?areaId=' + areaId,
-		type:'GET',
-		datatype: "json",
-		data:{
-			action: 'create'
-		},
-		success:function(result){
-			var columnNames = ["Nimi","Number","Erakond","Tulemus"];
-			var table = $("<table>").addClass("tablesorter");
-    		table.addClass("candidatesTable");
-    		var header = $("<tr>");
-    		var thead = $("<thead>");
-    		var tbody = $("<tbody>");
-			var row = $("<tr>");
-			
-			$.each(columnNames, function(index, value){
-				header.append($("<th>").text(value));
-			});
-			thead.append(header);
-			table.append(thead);
-			result.forEach(function (AreaResult) {
-    			row = $("<tr>").append($("<td>").text(AreaResult.name),
-    									$("<td>").text(AreaResult.candidateNumber),
-    									$("<td>").text(AreaResult.partyName),
-    									$("<td>").text(AreaResult.votes));
-    			tbody.append(row);
-			});
-			$("#content table").hide();
-			table.append(tbody);
-			$(".candidatesTable").remove();
-			table.prependTo("#content");
-			
-			$(".tablesorter").tablesorter({sortList: [[0,0], [1,0]]});
-		}
-	});
-}
-
 function createPartyResultsTable(partyId) {
 	$.ajax({
 		url:'/results/party?partyId=' + partyId,
